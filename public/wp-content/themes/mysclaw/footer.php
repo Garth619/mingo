db40525a518808f04594adda076d5f30828a6c05
Garrett Cullen <Garth619@users.noreply.github.com>
2021-11-18 20:32:34 -0800

    Push

--- public/wp-content/themes/mysclaw/footer.php
+++ public/wp-content/themes/mysclaw/footer.php
@@ -1,5 +1,14 @@
 <footer id="consultation">
 
+  <picture>
+
+    <source media='(min-width: 1170px)' data-srcset='<?php bloginfo('template_directory');?>/images/bkg-ft.jpg'>
+
+    <img id='footer-bg' class='lazyload' src='<?php bloginfo('template_directory');?>/images/mobile-placeholder.png'
+      alt='' />
+
+  </picture>
+
   <div id='footer-inner'>
 
     <div id='footer-form'>
@@ -10,17 +19,28 @@
 
     </div><!-- footer-form -->
 
-    <div id='footer-address'>
+    <div id='footer-contact'>
 
-    </div><!-- footer-address -->
+      <span id='footer-address'>330 East Kilbourn Ave<br> Suite 1221<br> Milwaukee, WI 53202</span>
+      <!-- footer-address -->
+
+      <a id='footer-phone' href='tel:+14142737400'>(414) 273-7400</a><!-- footer-phone -->
+
+    </div><!-- footer-contact -->
 
     <div id='copyright'>
 
-    </div><!-- copyright -->
+      <ul>
+        <li>&copy; <?php echo date('Y'); ?> Mingo & Yankala, S.C. All Rights Reserved.</li>
+        <li><a href=''>Privacy Policy</a></li>
+        <li><a href=''>Disclaimer</a></li>
+      </ul><!-- copyright -->
 
-    <a id='ilawermarketing' href='//ilawyermarketing.com' target='_blank' rel='noopener'>
-      <img src='<?php bloginfo('template_directory');?>/images/ilawyer.svg' alt='' />
-    </a><!-- ilawermarketing -->
+      <a id='ilawermarketing' href='//ilawyermarketing.com' target='_blank' rel='noopener'>
+        <img src='<?php bloginfo('template_directory');?>/images/ilawyer.svg' alt='' />
+      </a><!-- ilawermarketing -->
+
+    </div><!-- copyright -->
 
   </div><!-- footer-inner -->
 
