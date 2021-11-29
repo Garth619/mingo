cc42ba472cdcad7d33e3e7abdbd7c91d15955352
Garrett Cullen <Garth619@users.noreply.github.com>
2021-11-29 14:48:49 -0800

    Push

--- public/wp-content/themes/mysclaw/page-templates/includes/homepage_template_parts/section-3.php
+++ public/wp-content/themes/mysclaw/page-templates/includes/homepage_template_parts/section-3.php
@@ -1,3 +1,33 @@
 <section id='section-three'>
 
+  <div id='sec-three-inner'>
+
+    <div id='sec-three-image-wrapper'>
+
+      <img src='<?php bloginfo('template_directory');?>/images/img-1.jpg' alt='' />
+
+    </div><!-- sec-three-image-wrapper -->
+
+    <div id='sec-three-content' class='content'>
+
+      <h1 id='sec-three-title'>Milwaukee Personal Injury Attorneys</h1>
+
+      <h2>Lorem ipsum dolor sit amet, dignissim vel tellus eu, malesuada faucibus massa. Nam venenatis lacus ac eleifend
+        malesuad.
+      </h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis lectus, dignissim vel tellus eu,
+        malesuada faucibus massa. Nam venenatis lacus ac eleifend lacinia. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Aliquam in sem venenatis, rutrum dui at, commodo metus. </p>
+
+      <p>Aliquam pellentesque enim urna, id laoreet justo viverra vitae. Proin eleifend tortor consequat elementum
+        mattis. Sed blandit lacus a consequat iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Mauris ut purus sit amet tellus sollicitudin auctor. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Mauris tempus elementum neque ac imperdiet. Nullam porttitor aliquet faucibus.
+      </p>
+
+    </div><!-- sec-three-content-->
+
+  </div><!-- sec-three-inner -->
+
 </section><!-- section-three -->
