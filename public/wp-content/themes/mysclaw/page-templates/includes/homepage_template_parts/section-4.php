3983ae10e5e7fe76362076ad36e78a28c14ccc3d
Garrett Cullen <Garth619@users.noreply.github.com>
2021-11-29 18:30:04 -0800

    Push

--- public/wp-content/themes/mysclaw/page-templates/includes/homepage_template_parts/section-4.php
+++ public/wp-content/themes/mysclaw/page-templates/includes/homepage_template_parts/section-4.php
@@ -1,3 +1,68 @@
 <section id='section-four'>
 
+  <div id='sec-four-top'>
+
+    <div id='sec-four-header'>
+
+      <img src='<?php bloginfo('template_directory');?>/images/ico-wreath-L.svg' alt='' />
+
+      <span id='sec-four-subtitle'>Our Personal<br> Injury Guarantee</span>
+      <!-- sec-four-header -->
+
+      <img src='<?php bloginfo('template_directory');?>/images/ico-wreath-R.svg' alt='' />
+
+    </div><!-- sec-four-header -->
+
+    <div id='sec-four-intro'>
+
+      <p>If for any reason you’re not completely satisfied with the way your case is being handled within the first 30
+        days, you are free to take your case to another lawyer with no charge for costs expended and no fee.</p>
+
+      <p>If for any reason you’re not completely satisfied with the way your case is being handled within the first 30
+        days, you are free to take your case to another lawyer with no charge for costs expended and no fee.</p>
+
+      <a class='button-two sec-four-button' href=''>LEARN MORE</a><!-- button-one -->
+
+    </div><!-- sec-four-intro -->
+
+  </div><!-- sec-four-top -->
+
+  <div id='sec-four-content' class='content'>
+
+    <div class='sec-four-col'>
+
+      <h2>Lorem ipsum dolor sit amet, dignissim vel tellus eu, malesuada faucibus massa. </h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis lectus, dignissim vel tellus eu,
+        malesuada faucibus massa. Nam venenatis lacus ac eleifend lacinia. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Aliquam in sem venenatis, rutrum dui at, commodo metus.
+      </p>
+
+      <h2>Lorem ipsum dolor sit amet, dignissim vel tellus eu, malesuada faucibus massa. </h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis lectus, dignissim vel tellus eu,
+        malesuada faucibus massa. Nam venenatis lacus ac eleifend lacinia. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Aliquam in sem venenatis, rutrum dui at, commodo metus.
+      </p>
+
+    </div><!-- sec-four-col -->
+
+    <div class='sec-four-col'>
+
+      <h2>Lorem ipsum dolor sit amet, dignissim vel tellus eu, malesuada faucibus massa.</h2>
+
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas felis lectus, dignissim vel tellus eu,
+        malesuada faucibus massa. Nam venenatis lacus ac eleifend lacinia. Orci varius natoque penatibus et magnis dis
+        parturient montes, nascetur ridiculus mus. Aliquam in sem venenatis, rutrum dui at, commodo metus.</p>
+
+      <p>Aliquam pellentesque enim urna, id laoreet justo viverra vitae. Proin eleifend tortor consequat elementum
+        mattis. Sed blandit lacus a consequat iaculis. Interdum et malesuada fames ac ante ipsum primis in faucibus.
+        Mauris ut purus sit amet tellus sollicitudin auctor. Orci varius natoque penatibus et magnis dis parturient
+        montes, nascetur ridiculus mus. Mauris tempus elementum neque ac imperdiet. Nullam porttitor aliquet faucibus.
+      </p>
+
+    </div><!-- sec-four-col -->
+
+  </div><!-- sec-four-content -->
+
 </section><!-- section-four -->
