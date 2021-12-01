31d6137dd952ecf9943c2ad021e868582b48cc2e
Garrett Cullen <Garth619@users.noreply.github.com>
2021-11-30 17:44:58 -0800

    Push

---
+++ public/wp-content/themes/mysclaw/page-templates/includes/homepage_template_parts/section-6.php
@@ -0,0 +1,107 @@
+<div id='section-six'>
+
+  <div id='sec-six-top'>
+
+    <span id='sec-six-title'>Your case is only as good as the lawyer you hire.</span><!-- sec-six-title -->
+
+    <a class='button-two sec-six-button-two' href=''>Meet mark mingo </a>
+
+  </div><!-- sec-six-top -->
+
+  <div id='sec-six-content'>
+
+    <div class='sec-six-col'>
+
+      <div class='sec-six-sp'>
+
+        <span class='sec-six-sp-number'>01</span><!-- sec-six-sp-number -->
+
+        <div class='sec-six-sp-content'>
+
+          <span class='sec-six-sp-title'>We Know The Insurance
+            Company’s Next Move Before
+            They Do</span><!-- sec-six-sp-title -->
+
+          <span class='sec-six-sp-descrip'> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Integer fermentum semper
+            nibh, sed dapibus mauris pulvinar eget.
+          </span><!-- sec-six-sp-descrip -->
+
+          <a class='button-one sec-six-sp-button-one' href=''>Learn More</a>
+
+        </div><!-- sec-six-sp-content -->
+
+      </div><!-- sec-six-sp -->
+
+      <div class='sec-six-sp'>
+
+        <span class='sec-six-sp-number'>03</span><!-- sec-six-sp-number -->
+
+        <div class='sec-six-sp-content'>
+
+          <span class='sec-six-sp-title'>We Know The Insurance
+            Company’s Next Move Before
+            They Do</span><!-- sec-six-sp-title -->
+
+          <span class='sec-six-sp-descrip'> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Integer fermentum semper
+            nibh, sed dapibus mauris pulvinar eget.
+          </span><!-- sec-six-sp-descrip -->
+
+          <a class='button-one sec-six-sp-button-one' href=''>Learn More</a>
+
+        </div><!-- sec-six-sp-content -->
+
+      </div><!-- sec-six-sp -->
+
+    </div><!-- sec-six-col -->
+
+    <div class='sec-six-col'>
+
+      <div class='sec-six-sp'>
+
+        <span class='sec-six-sp-number'>02</span><!-- sec-six-sp-number -->
+
+        <div class='sec-six-sp-content'>
+
+          <span class='sec-six-sp-title'>We Know The Insurance
+            Company’s Next Move Before
+            They Do</span><!-- sec-six-sp-title -->
+
+          <span class='sec-six-sp-descrip'> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Integer fermentum semper
+            nibh, sed dapibus mauris pulvinar eget.
+          </span><!-- sec-six-sp-descrip -->
+
+          <a class='button-one sec-six-sp-button-one' href=''>Learn More</a>
+
+        </div><!-- sec-six-sp-content -->
+
+      </div><!-- sec-six-sp -->
+
+      <div class='sec-six-sp'>
+
+        <span class='sec-six-sp-number'>04</span><!-- sec-six-sp-number -->
+
+        <div class='sec-six-sp-content'>
+
+          <span class='sec-six-sp-title'>We Know The Insurance
+            Company’s Next Move Before
+            They Do</span><!-- sec-six-sp-title -->
+
+          <span class='sec-six-sp-descrip'> Lorem ipsum dolor sit amet, consectetur
+            adipiscing elit. Integer fermentum semper
+            nibh, sed dapibus mauris pulvinar eget.
+          </span><!-- sec-six-sp-descrip -->
+
+          <a class='button-one sec-six-sp-button-one' href=''>Learn More</a>
+
+        </div><!-- sec-six-sp-content -->
+
+      </div><!-- sec-six-sp -->
+
+    </div><!-- sec-six-col -->
+
+  </div><!-- sec-six-content -->
+
+</div><!-- section-six -->
